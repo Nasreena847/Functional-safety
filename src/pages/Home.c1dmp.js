483988b3f4a4719f7392b5c8e092819d776c961b
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2024-07-12 09:03:00 +0000

    Initial commit

---
+++ src/pages/Home.c1dmp.js
@@ -0,0 +1,45 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+    // Initially hide the menu
+    $w('#horizontalMenu1').hide();
+
+    // Show the menu after a delay (e.g., 2000ms or 2 seconds)
+    setTimeout(() => {
+        $w('#horizontalMenu1').show('fade', { duration: 200 });
+    }, 2000); // Adjust the delay time as needed
+});
+
+
+
+import wixWindow from 'wix-window';
+
+
+$w.onReady(function () {
+    // Function to check the screen width
+    function checkScreenWidth() {
+      wixWindow.getBoundingRect()
+        .then((windowSizeInfo) => {
+          let screenWidth = windowSizeInfo.window.width;
+          let targetWidth = 600; // Replace with your desired screen width in pixels
+  
+          if (screenWidth <= targetWidth) {
+            $w("#box41").hide();
+          } else {
+            $w("#box41").show();
+          }
+        });
+    }
+  
+    // Initial check when the page loads
+    checkScreenWidth();
+  
+    // Add an event listener for window resize using Wix's $w
+    $w("#page1").onViewportEnter(() => {
+      checkScreenWidth();
+    });
+  
+    $w("#page1").onViewportLeave(() => {
+      checkScreenWidth();
+    });
+  });
